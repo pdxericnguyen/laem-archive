9ce49b3677fce066a2eb295c7540717def3b1794
Eric Nguyen <pdxericnguyen@users.noreply.github.com>
2026-04-16 20:52:19 -0700

    Darken buy now hover treatment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         silver: {
           DEFAULT: "#1F1F1F",
-          hover: "#5A5A5A",
+          hover: "#3A3A3A",
           active: "#111111",
           text: "#FAFAFA",
           border: "#1F1F1F",
